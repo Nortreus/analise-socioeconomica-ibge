c541fe96ba5ae28dc3b9c334f84c61c912721210
Rafael Assunção <62725031+Nortreus@users.noreply.github.com>
2026-08-20 11:44:28 -0300

    Update script.py

--- script.py
+++ script.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg') 
 from scipy import stats
 
 def obter_dados_ibge(url_api):
